87dcae465b1025db657862f127f83c6628710a54
Burak Seyhan <seyhanburak9@yahoo.com>
2019-05-09 18:21:36 +1000

    change default route

--- demeter/demeter/App_Start/RouteConfig.cs
+++ demeter/demeter/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace demeter
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Welcome", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "LandingPage", id = UrlParameter.Optional }
             );
         }
     }
